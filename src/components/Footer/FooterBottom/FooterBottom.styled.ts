281f61f2bf7819de83c36c58135cc77eae971612
PhanLong166 <longbpse170607@fpt.edu.vn>
2024-05-20 22:05:47 +0700

    ADD - Footer Bottom

--- src/components/Footer/FooterBottom/FooterBottom.styled.ts
+++ src/components/Footer/FooterBottom/FooterBottom.styled.ts
@@ -7,15 +7,40 @@ export const BottomContainer = styled.div`
     align-items: center;
 
     background-color: ${theme.color.primary};
-    width: 1440px;
+    border-bottom: 2px solid ${theme.color.white};
+    
 `;
 
 export const BottomFlexbox = styled.div`
     display: flex;
     justify-content: space-between;
+    width: 1440px;
+    padding: 10px;
 `;
 
-export const LogoName = styled.h1`
+export const LogoName = styled.p`
     font-family: "Great Vibes", cursive;
     font-style: normal;
+    font-size: 48px;
+
+    color: ${theme.color.white};
+`;
+export const PolicyArea = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 20px;
+`;
+
+export const PolicyElements = styled.div`
+    font-family: "Poppins", sans-serif;
+    font-size: 16px;
+    color: ${theme.color.white};
+`;
+
+export const CopyrightFooter = styled.div`
+    background-color: ${theme.color.primary};
+    text-align: center;
+    font-size: 16px;
+    padding: 10px;
+    color: ${theme.color.white};
 `;
--- src/components/Footer/FooterBottom/FooterBottom.tsx
+++ src/components/Footer/FooterBottom/FooterBottom.tsx
@@ -8,8 +8,16 @@ const FooterBottom = () => {
                     <Styled.LogoName>
                         Aphromas Diamond
                     </Styled.LogoName>
+                    <Styled.PolicyArea>
+                        <Styled.PolicyElements>Terms & Conditions</Styled.PolicyElements>
+                        <Styled.PolicyElements>Privacy Policy</Styled.PolicyElements>
+                        <Styled.PolicyElements>Accessibility</Styled.PolicyElements>
+                    </Styled.PolicyArea>
                 </Styled.BottomFlexbox>
             </Styled.BottomContainer>
+            <Styled.CopyrightFooter>
+                Copyright © 2024
+            </Styled.CopyrightFooter>
         </>
     );
 }
