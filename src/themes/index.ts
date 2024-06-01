70e33f708ab7f8f3e3e80cb0878d9016dbc5cff5
PhanLong166 <longbpse170607@fpt.edu.vn>
2024-06-01 12:52:32 +0700

    UPDATE - tsconfig

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,5 +1,5 @@
 const PRIMARY_COLOR: string = '#102C57';
-const SECONDARY_COLOR: string = '#DAC0A3';
+const SECONDARY_COLOR: string = '#FFFFFF';
 const THIRD_COLOR: string = '#EADBC8';
 const FORTH_COLOR: string = '#F8F0E5';
 const FIFTH_COLOR: string = '#FFF7E8';
