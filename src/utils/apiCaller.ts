1483c76fc1872ff67849f970f188b28cd6eeb388
PhanLong166 <longbpse170607@fpt.edu.vn>
2024-06-01 16:26:11 +0700

    feat: 🎸 ADD - Axios HTTP request function

--- src/utils/apiCaller.ts
+++ src/utils/apiCaller.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 import config from "@/config";
+import cookieUtils from "./cookieUtils";
 
 export const request = (
     endpoint: string, 
@@ -8,14 +9,49 @@ export const request = (
     params: object = {},
     body: object = {},
 ): Promise<AxiosResponse> => {
+    const token = cookieUtils.getToken();
+    
     return axios({
         url: config.publicRuntime.API_URL + endpoint,
         method: method,
-        headers: Object.assign({}, headers, {Authorization: `Bearer `}),
+        headers: Object.assign({}, headers, {Authorization: `Bearer ${token}`}),
         params: Object.assign(params),
         data: body,
     });
 }
 
+export const get = (
+    endpoint: string,
+    params: object = {},
+    headers: object = {},
+): Promise<AxiosResponse> => {
+    return request(endpoint, 'GET', headers, params);
+}
 
+export const post = (
+    endpoint: string,
+    body: object = {},
+    params: object = {},
+    headers: object = {},
+): Promise<AxiosResponse> => {
+    return request(endpoint, 'POST', headers, params, body);
+};
+
+export const put = (
+    endpoint: string,
+    body: object = {},
+    params: object = {},
+    headers: object = {},
+): Promise<AxiosResponse> => {
+    return request(endpoint, 'PUT', headers, params, body);
+}
+
+export const remove = (
+    endpoint: string, 
+    body: object = {},
+    params: object = {},
+    headers: object = {}
+): Promise<AxiosResponse> => {
+    return request(endpoint, 'DELETE', headers, params, body);
+}
 
