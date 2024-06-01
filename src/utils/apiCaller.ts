bc79d9a0a223bf08f14d0ede66605f8f8a57100b
PhanLong166 <longbpse170607@fpt.edu.vn>
2024-06-01 15:46:46 +0700

    feat: 🎸 Add git cz

--- src/utils/apiCaller.ts
+++ src/utils/apiCaller.ts
@@ -1,7 +1,17 @@
-import axios from "axios";
-import config from "../config";
+import axios, { AxiosResponse } from "axios";
+import config from "@/config";
 
+export const request = (
+    endpoint: string, 
+    method: string,
+    headers: object = {},
+    params: object = {},
+    body: object = {},
+): Promise<AxiosResponse> => {
+    return axios({
 
+    });
+}
 
 
 
