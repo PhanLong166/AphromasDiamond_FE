2c32a13fbd05058b2c8c17b05beca3e8312c983a
PhanLong166 <longbpse170607@fpt.edu.vn>
2024-06-09 13:53:13 +0700

    feat: 🎸 ADD - Breakpoint theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import GlobalStyles from './themes/globalStyles.ts'
+import { DefaultTheme, ThemeProvider } from 'styled-components';
+import { createStyledBreakpointsTheme } from 'styled-breakpoints';
+
+export const breakpoints = {
+  xs: '360px',
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px',
+  xxl: '1400px',
+} as const;
+
+const theme: DefaultTheme = createStyledBreakpointsTheme({
+  breakpoints
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-    <GlobalStyles />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>,
 )
