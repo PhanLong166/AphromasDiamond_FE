357d510907e3a640d1da0fad361600facbe441e4
lphuocloc <lephuocloc2003@gmail.com>
2024-06-30 14:28:06 +0700

    FIX - Account

--- src/pages/Customer/AccountDetail/AccountDetail.tsx
+++ src/pages/Customer/AccountDetail/AccountDetail.tsx
@@ -1,12 +1,12 @@
-import styled from 'styled-components';
-import { ChangeEvent, FormEvent, useState, useRef, useEffect } from 'react';
-import AccountCus from '@/components/Customer/Account Details/AccountCus';
+import styled from "styled-components";
+import { ChangeEvent, FormEvent, useState, useRef, useEffect } from "react";
+import AccountCus from "@/components/Customer/Account Details/AccountCus";
 
 interface Address {
   street: string;
   city: string;
-  // phone: string;
-  country: string;
+  ward: string;
+  district: string;
 }
 
 interface Account {
@@ -22,18 +22,18 @@ const Account = () => {
   const [isAccountModalOpen, setAccountModalOpen] = useState(false);
 
   const [address, setAddress] = useState<Address>({
-    street: '191-103 Integer Rd.',
-    city: 'Forrest Ray',
-    // phone: '(404) 960-3807',
-    country: 'Mexico',
+    street: "191-103 Integer Rd.",
+    city: "Forrest Ray",
+    ward: "Tay Thanh",
+    district: "10",
   });
 
   const [account, setAccount] = useState<Account>({
-    firstName: 'Le Phuoc',
-    lastName: 'Loc',
-    phone: '(404) 960-3807',
-    email: 'loclpse171201@gmail.com',
-    password: '*************',
+    firstName: "Le Phuoc",
+    lastName: "Loc",
+    phone: "0123456789",
+    email: "loclpse171201@gmail.com",
+    password: "*************",
   });
 
   const [tempAddress, setTempAddress] = useState<Address>({ ...address });
@@ -48,36 +48,40 @@ const Account = () => {
     const handleOutsideClick = (event: MouseEvent) => {
       // Kiểm tra nếu click ra ngoài phạm vi modal
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      if (modalRef.current && !(modalRef.current as any).contains(event.target)) {
+      if (
+        modalRef.current &&
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        !(modalRef.current as any).contains(event.target)
+      ) {
         closeModal();
       }
     };
 
     // Thêm sự kiện mousedown khi component được mount
-    document.addEventListener('mousedown', handleOutsideClick);
+    document.addEventListener("mousedown", handleOutsideClick);
 
     // Xóa sự kiện khi component bị unmount
     return () => {
-      document.removeEventListener('mousedown', handleOutsideClick);
+      document.removeEventListener("mousedown", handleOutsideClick);
     };
-  }, ); // [] đảm bảo useEffect chỉ chạy một lần khi component mount
+  }); // [] đảm bảo useEffect chỉ chạy một lần khi component mount
 
   const validateAddress = (address: Address) => {
     const errors: Partial<Address> = {};
-    if (!address.street) errors.street = 'Street address is required';
-    if (!address.city) errors.city = 'City is required';
-    // if (!address.phone) errors.phone = 'Phone number is required';
-    if (!address.country) errors.country = 'Country is required';
+    if (!address.street) errors.street = "Street address is required";
+    if (!address.city) errors.city = "City is required";
+    if (!address.ward) errors.ward = "Ward is required";
+    if (!address.district) errors.district = "District is required";
     return errors;
   };
 
   const validateAccount = (account: Account) => {
     const errors: Partial<Account> = {};
-    if (!account.firstName) errors.firstName = 'First name is required';
-    if (!account.lastName) errors.lastName = 'Last name is required';
-    if (!account.phone) errors.phone = 'Phone number is required';
-    if (!account.email) errors.email = 'Email is required';
-    if (!account.password) errors.password = 'Password is required';
+    if (!account.firstName) errors.firstName = "First name is required";
+    if (!account.lastName) errors.lastName = "Last name is required";
+    if (!account.phone) errors.phone = "Phone number is required";
+    if (!account.email) errors.email = "Email is required";
+    if (!account.password) errors.password = "Password is required";
     return errors;
   };
 
@@ -123,18 +127,18 @@ const Account = () => {
       [name]: value,
     }));
 
-     // Clear the error for the email field if it is valid
-  if (name === 'email' && validateEmail(value)) {
-    setAccountErrors((prevErrors) => ({
-      ...prevErrors,
-      email: undefined,
-    }));
-  } else if (name === 'email') {
-    setAccountErrors((prevErrors) => ({
-      ...prevErrors,
-      // email: 'Invalid email format',
-    }));
-  }
+    // Clear the error for the email field if it is valid
+    if (name === "email" && validateEmail(value)) {
+      setAccountErrors((prevErrors) => ({
+        ...prevErrors,
+        email: undefined,
+      }));
+    } else if (name === "email") {
+      setAccountErrors((prevErrors) => ({
+        ...prevErrors,
+        // email: 'Invalid email format',
+      }));
+    }
   };
 
   const handleAddressSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -153,13 +157,13 @@ const Account = () => {
     const errors = validateAccount(tempAccount);
     const emailIsValid = validateEmail(tempAccount.email);
 
-  if (!emailIsValid) {
-    errors.email = 'Email is required';
-  }
-  // Check if phone number is exactly 10 digits and starts with 0
-  if (tempAccount.phone.length !== 10 || !tempAccount.phone.startsWith('0')) {
-    errors.phone = 'Phone number must be exactly 10 digits and start with 0';
-  }
+    if (!emailIsValid) {
+      errors.email = "Email is required";
+    }
+    // Check if phone number is exactly 10 digits and starts with 0
+    if (tempAccount.phone.length !== 10 || !tempAccount.phone.startsWith("0")) {
+      errors.phone = "Phone number must be exactly 10 digits and start with 0";
+    }
 
     if (Object.keys(errors).length === 0) {
       setAccount(tempAccount);
@@ -171,17 +175,17 @@ const Account = () => {
 
   const handlePhoneChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-  
+
     // Allow only numeric input
-    const numericValue = value.replace(/[^0-9]/g, '');
-  
+    const numericValue = value.replace(/[^0-9]/g, "");
+
     setTempAccount((prevAccount) => ({
       ...prevAccount,
       [name]: numericValue,
     }));
-  
+
     // Validate phone number length and starting digit
-    if (numericValue.length === 10 && numericValue.startsWith('0')) {
+    if (numericValue.length === 10 && numericValue.startsWith("0")) {
       setAccountErrors((prevErrors) => ({
         ...prevErrors,
         phone: undefined,
@@ -189,7 +193,7 @@ const Account = () => {
     } else {
       setAccountErrors((prevErrors) => ({
         ...prevErrors,
-        phone: 'Phone number must be exactly 10 digits and start with 0',
+        phone: "Phone number must be exactly 10 digits and start with 0",
       }));
     }
   };
@@ -207,6 +211,7 @@ const Account = () => {
                   <InfoTitle>Address Delivery</InfoTitle>
                   <EditButton onClick={handleAddressEdit}>Edit</EditButton>
                 </Row>
+               <form action="" >
                 <Row>
                   <Column>
                     <DetailGroup>
@@ -217,20 +222,26 @@ const Account = () => {
                       <Label>CITY</Label>
                       <Detail>{address.city}</Detail>
                     </DetailGroup>
+                    <DetailGroup>
+                      <Label>DISTRICT</Label>
+                      <Detail>{address.district}</Detail>
+                    </DetailGroup>
                   </Column>
                   <Column>
                     <DetailGroup>
-                      <Label>COUNTRY</Label>
-                      <Detail>{address.country}</Detail>
+                      <Label>WARD</Label>
+                      <Detail>{address.ward}</Detail>
                     </DetailGroup>
                   </Column>
                 </Row>
+                </form>
               </Column>
               <Column>
                 <Row>
                   <InfoTitle>Account Details</InfoTitle>
                   <EditButton onClick={handleAccountEdit}>Edit</EditButton>
                 </Row>
+                <form>
                 <Row>
                   <Column>
                     <DetailGroup>
@@ -257,6 +268,7 @@ const Account = () => {
                     </DetailGroup>
                   </Column>
                 </Row>
+                </form>
               </Column>
             </InfoContainer>
           </InfoSection>
@@ -289,19 +301,35 @@ const Account = () => {
                 {addressErrors.city && <Error>{addressErrors.city}</Error>}
               </label>
               <label>
-                Country:
+                District:
                 <input
                   type="text"
                   name="country"
-                  value={tempAddress.country}
+                  value={tempAddress.district}
                   onChange={handleAddressChange}
                 />
                 {/* {tempAccount.country.length < 2 && <Error>Country must be at least 2 characters.</Error>} */}
-                {addressErrors.country && <Error>{addressErrors.country}</Error>}
+                {addressErrors.district && (
+                  <Error>{addressErrors.district}</Error>
+                )}
+              </label>
+              <label>
+                Ward:
+                <input
+                  type="text"
+                  name="ward"
+                  value={tempAddress.ward}
+                  onChange={handleAddressChange}
+                />
+                {addressErrors.ward && (
+                  <Error>{addressErrors.ward}</Error>
+                )}
               </label>
               <ModalActions>
-                <button type="submit">Save</button>
-                <button type="button" onClick={closeModal}>Cancel</button>
+                <button className="save-button" type="submit">Save</button>
+                <button className="cancel-button" type="button" onClick={closeModal}>
+                  Cancel
+                </button>
               </ModalActions>
             </form>
           </ModalContent>
@@ -321,7 +349,9 @@ const Account = () => {
                   value={tempAccount.firstName}
                   onChange={handleAccountChange}
                 />
-                {accountErrors.firstName && <Error>{accountErrors.firstName}</Error>}
+                {accountErrors.firstName && (
+                  <Error>{accountErrors.firstName}</Error>
+                )}
               </label>
               <label>
                 Last Name:
@@ -331,7 +361,9 @@ const Account = () => {
                   value={tempAccount.lastName}
                   onChange={handleAccountChange}
                 />
-                {accountErrors.lastName && <Error>{accountErrors.lastName}</Error>}
+                {accountErrors.lastName && (
+                  <Error>{accountErrors.lastName}</Error>
+                )}
               </label>
               <label>
                 Phone:
@@ -353,7 +385,9 @@ const Account = () => {
                   onChange={handleAccountChange}
                 />
                 {accountErrors.email && <Error>{accountErrors.email}</Error>}
-                {!validateEmail(tempAccount.email) && <Error>Invalid email format. Example: 0Hb7W@gmail.com</Error>}
+                {!validateEmail(tempAccount.email) && (
+                  <Error>Invalid email format. Example: 0Hb7W@gmail.com</Error>
+                )}
               </label>
               <label>
                 Current Password:
@@ -363,12 +397,13 @@ const Account = () => {
                   value={tempAccount.password}
                   onChange={handleAccountChange}
                 />
-                {accountErrors.password && <Error>{accountErrors.password}</Error>}
+                {accountErrors.password && (
+                  <Error>{accountErrors.password}</Error>
+                )}
               </label>
               <ModalActions>
-                
-                <button type="submit">Save</button>
-                <button  type="button" onClick={closeModal}>Cancel</button>
+                <button className="save-button" type="submit">Save</button>
+                <button className="cancel-button" type="button" onClick={closeModal}>Cancel</button>
               </ModalActions>
             </form>
           </ModalContent>
@@ -398,7 +433,7 @@ const Section = styled.section`
   color: #000;
   justify-content: center;
   margin: 0 auto;
-  font: 400 15px / 150% 'Crimson Text', sans-serif;
+  font: 400 15px / 150% "Crimson Text", sans-serif;
   margin-bottom: 150px;
   width: 1400px;
   @media (max-width: 991px) {
@@ -410,7 +445,7 @@ const Section = styled.section`
 const ProfileTitle = styled.div`
   position: relative;
   margin: 15px 0 87px;
-  font: 600 32px 'Crimson Text', sans-serif;
+  font: 600 32px "Crimson Text", sans-serif;
   display: flex;
   justify-content: space-around;
   @media (max-width: 991px) {
@@ -452,10 +487,10 @@ const Column = styled.div`
 `;
 
 const InfoTitle = styled.div`
-  font-family: 'Crimson Text', sans-serif;
+  font-family: "Crimson Text", sans-serif;
   font-size: 25px;
   font-weight: 600;
-  
+
   /* margin: 0 0 25px; */
 `;
 
@@ -465,20 +500,17 @@ const DetailGroup = styled.div`
   margin-bottom: 35px;
 `;
 
-
 const Detail = styled.span`
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
   font-weight: 400;
   letter-spacing: 0.75px;
   margin-top: 6px;
   color: rgb(0 0 10 / 55%);
-line-height: 30px;
-
-
+  line-height: 30px;
 `;
 
 const Label = styled.label`
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
   padding-top: 22px;
   font-size: 17px;
 `;
@@ -535,12 +567,12 @@ const ModalContent = styled.div`
   form {
     display: grid;
     grid-template-columns: repeat(1, 1fr);
-    gap: 20px; 
+    gap: 20px;
   }
 
   label {
     display: block;
-    font-family: 'Poppins', sans-serif;
+    font-family: "Poppins", sans-serif;
     margin-bottom: 5px;
   }
 
@@ -550,7 +582,7 @@ const ModalContent = styled.div`
     border: 1px solid #ccc;
     border-radius: 5px;
     box-sizing: border-box;
-    font-family: 'Poppins', sans-serif;
+    font-family: "Poppins", sans-serif;
     margin-top: 5px;
   }
   h2 {
@@ -568,11 +600,11 @@ const ModalActions = styled.div`
   align-content: center;
   flex-wrap: wrap;
 
-  button {
+  .save-button {
     font-size: 12px;
     padding: 10px 20px;
-    background-color: #fff9f7;
-    color: #151542;
+    background-color: #28a745;
+    color: #fff;
     border: none;
     border: 1px solid #151542;
     cursor: pointer;
@@ -583,15 +615,34 @@ const ModalActions = styled.div`
     width: 100%;
 
     &:hover {
-      background-color: #102c57;
+      background-color: #218838;;
       color: #fff;
       transition: all 0.45s ease;
     }
-    &:hover:not(:first-of-type) {
-      background-color: #fd424b; 
-      color: #fff; 
-    }
   }
+  .cancel-button{
+    font-size: 12px;
+    padding: 10px 20px;
+    background-color: #ff4757;
+    color: #fff;
+    border: none;
+    border: 1px solid #151542;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    font-family: "Gantari", sans-serif;
+    font-weight: 600;
+    transition: all 0.45s ease;
+    width: 100%;
+
+    &:hover {
+      background-color: #c0392b;;
+      color: #fff;
+      transition: all 0.45s ease;
+    }
+
+  }
+    
+  
 `;
 
 const Error = styled.span`
