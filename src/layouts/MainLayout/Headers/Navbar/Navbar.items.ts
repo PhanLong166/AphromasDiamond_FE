cde303c34231ba98faba118d0002a5774eb4c430
PhanLong166 <longbpse170607@fpt.edu.vn>
2024-07-01 08:39:10 +0700

    refactor: 💡 UPDATE - Content for navbar

--- src/layouts/MainLayout/Headers/Navbar/Navbar.items.ts
+++ src/layouts/MainLayout/Headers/Navbar/Navbar.items.ts
@@ -118,20 +118,20 @@ export const items: MenuItem[] = [
         label: 'Engagement Ring',
         children: [
             {
-                key: 'jewelry-type',
+                key: 'engagement-ring',
                 label: 'Jewelry Classification',
                 type: 'group',
                 children: [
-                    {key: 'jewelry-type-1', label: 'Round'},
-                    {key: 'jewelry-type-2', label: 'Princess'},
-                    {key: 'jewelry-type-3', label: 'Cushion'},
-                    {key: 'jewelry-type-4', label: 'Oval'},
-                    {key: 'jewelry-type-5', label: 'Emerald'},
-                    {key: 'jewelry-type-6', label: 'Pear'},
-                    {key: 'jewelry-type-7', label: 'Asscher'},
-                    {key: 'jewelry-type-8', label: 'Heart'},
-                    {key: 'jewelry-type-9', label: 'Radient'},
-                    {key: 'jewelry-type-10 ', label: 'Marquise'},
+                    {key: 'engagement-ring-1', label: 'Round'},
+                    {key: 'engagement-ring-2', label: 'Princess'},
+                    {key: 'engagement-ring-3', label: 'Cushion'},
+                    {key: 'engagement-ring-4', label: 'Oval'},
+                    {key: 'engagement-ring-5', label: 'Emerald'},
+                    {key: 'engagement-ring-6', label: 'Pear'},
+                    {key: 'engagement-ring-7', label: 'Asscher'},
+                    {key: 'engagement-ring-8', label: 'Heart'},
+                    {key: 'engagement-ring-9', label: 'Radient'},
+                    {key: 'engagement-ring-10 ', label: 'Marquise'},
                 ]
             },
             {
@@ -228,59 +228,118 @@ export const items: MenuItem[] = [
         ]
     },
     {
-        key: 'diamond-ring',
-        label: 'Diamond Ring',
+        key: 'jewelry',
+        label: 'Jewelry',
         children: [
             {
-                key: 'ring-by-shape',
-                label: 'Diamond Ring By Shape',
+                key: 'jewelry-type',
+                label: 'Jewelry Classification',
                 type: 'group',
                 children: [
-                    {key: 'ring-shape-1', label: 'Round'},
-                    {key: 'ring-shape-2', label: 'Princess'},
-                    {key: 'ring-shape-3', label: 'Cushion'},
-                    {key: 'ring-shape-4', label: 'Oval'},
-                    {key: 'ring-shape-5', label: 'Emerald'},
-                    {key: 'ring-shape-6', label: 'Pear'},
-                    {key: 'ring-shape-7', label: 'Asscher'},
-                    {key: 'ring-shape-8', label: 'Heart'},
-                    {key: 'ring-shape-9', label: 'Radient'},
-                    {key: 'ring-shape-10', label: 'Marquise'},
+                    {key: 'jewelry-type-1', label: 'Rings'},
+                    {key: 'jewelry-type-2', label: 'Necklaces'},
+                    {key: 'jewelry-type-3', label: 'Earings'},
+                    {key: 'jewelry-type-4', label: 'Bracelets'},
+                    {key: 'jewelry-type-5', label: 'Anklets'},
+                    {key: 'jewelry-type-6', label: 'Bangles'},
+                    {key: 'jewelry-type-7', label: 'Chokers'},
+                    {key: 'jewelry-type-8', label: 'Pendants'},
                 ]
             },
             {
-                key: 'ring-metals',
-                label: 'Ring metals',
+                key: 'jewelry-metals',
+                label: 'Jewelry metals',
                 type: 'group',
                 children: [
-                    {key: 'ring-metal-1', label: 'White Gold'},
-                    {key: 'ring-metal-2', label: 'Yellow Gold'},
-                    {key: 'ring-metal-3', label: 'Rose Gold'},
-                    {key: 'ring-metal-4', label: 'Platinum'},
+                    {key: 'jewelry-metals-1', label: 'White Gold'},
+                    {key: 'jewelry-metal-2', label: 'Yellow Gold'},
+                    {key: 'jewelry-metal-3', label: 'Rose Gold'},
+                    {key: 'jewelry-metal-4', label: 'Platinum'},
                 ]
             },
             {
-                key: 'ring-designer',
-                label: 'Designer Ring',
+                key: 'jewelry-firm',
+                label: 'Jewelry Firm',
                 type: 'group',
                 children: [
-                    {key: 'ring-designer-1', label: 'Zac Zac Posen'},
-                    {key: 'ring-designer-2', label: 'Bella Vaughan'},
-                    {key: 'ring-designer-3', label: 'Blue Nile Studio'},
-                    {key: 'ring-designer-4', label: 'The Gallery Collection'},
+                    {key: 'jewelry-firm-1', label: 'Van Cleef & Arpels'},
+                    {key: 'jewelry-firm-2', label: 'Harry Winston'},
+                    {key: 'jewelry-firm-3', label: 'Cartier'},
+                    {key: 'jewelry-firm-4', label: 'Tiffany & Co'},
+                    {key: 'jewelry-firm-5', label: 'Bvlgari'},
                 ]
             },
             {
                 key: 'learn-about',
-                label: 'Diamond Ring By Shape',
+                label: 'Learn About',
                 type: 'group',
                 children: [
-                    {key: 'learn-1', label: 'Find Your Ring Size'},
+                    {key: 'learn-1', label: 'Buying Guide'},
                     {key: 'learn-2', label: 'Learn About the 4Cs'},
-                    {key: 'learn-3', label: 'Buying Guide'},
-                    {key: 'learn-4', label: 'Metal Education'},
+                    {key: 'learn-3', label: 'Jewelry Education'},
                 ]
             }
         ]
+    },
+    {
+        key: 'gift',
+        label: 'Gifts',
+        children: [
+            {
+                key: 'jewelry-type',
+                label: 'Jewelry Classification',
+                type: 'group',
+                children: [
+                    {key: 'jewelry-type-1', label: 'Rings'},
+                    {key: 'jewelry-type-2', label: 'Necklaces'},
+                    {key: 'jewelry-type-3', label: 'Earings'},
+                    {key: 'jewelry-type-4', label: 'Bracelets'},
+                    {key: 'jewelry-type-5', label: 'Anklets'},
+                    {key: 'jewelry-type-6', label: 'Bangles'},
+                    {key: 'jewelry-type-7', label: 'Chokers'},
+                    {key: 'jewelry-type-8', label: 'Pendants'},
+                ]
+            },
+            {
+                key: 'ring',
+                label: 'Ring',
+                type: 'group',
+                children: [
+                    {key: 'ring-1', label: 'Diamond Ring'},
+                    {key: 'ring-2', label: 'Engagement Ring'},
+                    {key: 'ring-3', label: 'Wedding Ring'},
+                ]
+            },
+            {
+                key: 'jewelry-firm',
+                label: 'Jewelry Firm',
+                type: 'group',
+                children: [
+                    {key: 'jewelry-firm-1', label: 'Van Cleef & Arpels'},
+                    {key: 'jewelry-firm-2', label: 'Harry Winston'},
+                    {key: 'jewelry-firm-3', label: 'Cartier'},
+                    {key: 'jewelry-firm-4', label: 'Tiffany & Co'},
+                    {key: 'jewelry-firm-5', label: 'Bvlgari'},
+                ]
+            },
+            {
+                key: 'learn-about',
+                label: 'Learn About',
+                type: 'group',
+                children: [
+                    {key: 'learn-1', label: 'Buying Guide'},
+                    {key: 'learn-2', label: 'Learn About the 4Cs'},
+                    {key: 'learn-3', label: 'Jewelry Education'},
+                ]
+            }
+        ]
+    },
+    {
+        key: 'learn-about',
+        label: 'Learn About'
+    },
+    {
+        key: 'about-us',
+        label: 'About Us'
     }
 ]
