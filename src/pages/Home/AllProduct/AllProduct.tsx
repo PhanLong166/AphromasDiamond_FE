0e21f6d8f5024b34797f1797472d8ee9645bd662
PhanLong166 <longbpse170607@fpt.edu.vn>
2024-07-02 14:46:45 +0700

    refactor: 💡 UPDATE - public routes

--- src/pages/Home/AllProduct/AllProduct.tsx
+++ src/pages/Home/AllProduct/AllProduct.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import { Breadcrumb } from "antd";
 import { products } from "./../shared/ListOfProducts";
 
-import {Button} from "antd";
 import {
   HeartFilled,
   HeartOutlined,
