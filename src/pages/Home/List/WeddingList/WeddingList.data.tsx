3e835edcecf372d2265d9429f78f202dcfdf60d8
nhunguyenenn <nguyentnnse170598@fpt.edu.vn>
2024-07-12 02:01:31 +0700

    SEPARATE DATA - API SETUP

---
+++ src/pages/Home/List/WeddingList/WeddingList.data.tsx
@@ -0,0 +1,374 @@
+import { products } from "../../shared/ListOfProducts";
+
+export const jewelryWeddingData: Record<string, any> = {
+    "round-wedding-ring": {
+      title: "Round Diamond Wedding Rings",
+      description:
+        "Propose with timeless style when you design a handcrafted ring with a round brilliant diamond. Round diamonds are the most popular choice at AD because of their traditional look and incredible sparkle. This shape is also the most versatile, pairing seamlessly with virtually every setting style offered at AD.",
+      products: products.filter(
+        (product) =>
+          product.categories === "wedding-ring" && product.shape === "round"
+      ),
+      faqs: [
+        {
+          key: "1",
+          label:
+            "Round Ring What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $900 to $1000 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2Fsnapedit_1719690260501.jpg?alt=media&token=a445aaed-698e-4696-a34a-39e978168f89",
+    },
+    "princess-wedding-ring": {
+      title: "Princess Diamond Wedding Rings",
+      description:
+        " Discover the allure of our princess-cut diamond rings, characterized by their clean lines and modern elegance. Available in yellow gold, white gold, rose gold, or platinum settings, these rings are meticulously crafted to capture the essence of grace and luxury, making them ideal for celebrating milestones and special moments. Each ring reflects precision craftsmanship and a contemporary aesthetic, perfect for those who appreciate refined beauty with a touch of glamour.",
+      products: products.filter(
+        (product) =>
+          product.categories === "wedding-ring" && product.shape === "princess"
+      ),
+      faqs: [
+        {
+          key: "1",
+          label:
+            "Princess Ring What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $700 to $800 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2Fsnapedit_1719690260501.jpg?alt=media&token=a445aaed-698e-4696-a34a-39e978168f89",
+    },
+    "heart-wedding-ring": {
+      title: "Heart Diamond Wedding Rings",
+      description:
+        "Symbolizing romance and devotion, our heart-shaped diamond rings are a testament to everlasting love. Expertly set in yellow gold, white gold, rose gold, or platinum, each ring is a blend of artistry and emotion, making it a perfect expression of love and affection. With their timeless design and exquisite detailing, these rings are cherished heirlooms that symbolize the deep bond between two individuals.",
+      products: products.filter(
+        (product) =>
+          product.categories === "wedding-ring" && product.shape === "heart"
+      ),
+      faqs: [
+        {
+          key: "1",
+          label:
+            "Heart Ring What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $500 to $600 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2Fsnapedit_1719690260501.jpg?alt=media&token=a445aaed-698e-4696-a34a-39e978168f89",
+    },
+    "oval-wedding-ring": {
+      title: "Oval Diamond Wedding Rings",
+      description:
+        "Embrace the distinctive beauty of our oval-shaped diamond rings, known for their timeless appeal and unique charm. Available in yellow gold, white gold, rose gold, or platinum settings, these rings exude sophistication and allure, making them a captivating choice for those who appreciate classic elegance with a modern twist. Each ring is meticulously crafted to enhance the diamond's natural brilliance and fire, creating a piece that stands out with understated elegance and grace.",
+      products: products.filter(
+        (product) =>
+          product.categories === "wedding-ring" && product.shape === "oval"
+      ),
+      faqs: [
+        {
+          key: "1",
+          label:
+            "Oval Ring What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $300 to $400 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2Fsnapedit_1719690260501.jpg?alt=media&token=a445aaed-698e-4696-a34a-39e978168f89",
+    },
+    "cushion-wedding-ring": {
+      title: "Cushion Diamond Wedding Rings",
+      description:
+        "Our cushion-cut diamond rings combine classic charm with contemporary flair. Featuring square-shaped diamonds set in yellow gold, white gold, rose gold, or platinum, these rings are crafted to highlight the diamond's brilliance and fire, making them a stunning choice for those who desire a blend of tradition and luxury. Each ring is designed with meticulous attention to detail, ensuring a piece that is both timeless and distinctive, perfect for making a statement of enduring elegance.",
+      products: products.filter(
+        (product) =>
+          product.categories === "wedding-ring" && product.shape === "cushion"
+      ),
+      faqs: [
+        {
+          key: "1",
+          label:
+            "Cushion Ring What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $276 to $56,024 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2Fsnapedit_1719690260501.jpg?alt=media&token=a445aaed-698e-4696-a34a-39e978168f89",
+    },
+    "emerald-wedding-ring": {
+      title: "Emerald Diamond Wedding Rings",
+      description:
+        "Make a statement with our emerald-cut diamond rings, renowned for their striking beauty and bold presence. Set in yellow gold, white gold, rose gold, or platinum, these rings showcase the diamond's clarity and geometric elegance, offering a timeless and sophisticated look for any occasion. Each ring is crafted with precision to enhance the diamond's natural allure, making it a symbol of luxury and refinement that will be cherished for generations.",
+      products: products.filter(
+        (product) =>
+          product.categories === "wedding-ring" && product.shape === "emerald"
+      ),
+      faqs: [
+        {
+          key: "1",
+          label:
+            "Emerald Ring What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $900 to $1000 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2Fsnapedit_1719690260501.jpg?alt=media&token=a445aaed-698e-4696-a34a-39e978168f89",
+    },
+    "asscher-wedding-ring": {
+      title: "Asscher Diamond Wedding Rings",
+      description:
+        "Experience luxury with our asscher-cut diamond rings, distinguished by their octagonal shape and Art Deco-inspired design. Crafted in yellow gold, white gold, rose gold, or platinum, these rings exude vintage charm and modern sophistication, making them a captivating choice for those who appreciate classic elegance with a touch of glamour. Each ring is a testament to superior craftsmanship and timeless design, ideal for marking life's most special moments with style and grace.",
+      products: products.filter(
+        (product) =>
+          product.categories === "wedding-ring" && product.shape === "asscher"
+      ),
+      faqs: [
+        {
+          key: "1",
+          label:
+            "Asscher Ring What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $700 to $800 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2Fsnapedit_1719690260501.jpg?alt=media&token=a445aaed-698e-4696-a34a-39e978168f89",
+    },
+    "marquise-wedding-ring": {
+      title: "Marquise Diamond Wedding Rings",
+      description:
+        "Celebrate elegance with our marquise-cut diamond rings, distinguished by their elongated shape and graceful curves. Available in yellow gold, white gold, rose gold, or platinum settings, these rings offer a unique blend of vintage charm and contemporary allure, making them an exquisite choice for those who seek timeless beauty and refined craftsmanship. Each ring is crafted with precision to accentuate the diamond's brilliance, creating a piece that embodies sophistication and grace.",
+      products: products.filter(
+        (product) =>
+          product.categories === "wedding-ring" && product.shape === "marquise"
+      ),
+      faqs: [
+        {
+          key: "1",
+          label:
+            "Marquise Ring What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $300 to $400 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2Fsnapedit_1719690260501.jpg?alt=media&token=a445aaed-698e-4696-a34a-39e978168f89",
+    },
+    "radiant-wedding-ring": {
+      title: "Radiant Diamond Wedding Rings",
+      description:
+        "Our radiant-cut diamond rings are designed to dazzle and delight. Featuring a rectangular shape that maximizes brilliance, these rings are set in yellow gold, white gold, rose gold, or platinum, capturing the essence of sophistication and luxury with every facet. Each ring is meticulously crafted to reflect the diamond's radiance and fire, creating a piece that stands out as a symbol of elegance and refinement, perfect for celebrating love and achievement.",
+      products: products.filter(
+        (product) =>
+          product.categories === "wedding-ring" && product.shape === "radiant"
+      ),
+      faqs: [
+        {
+          key: "1",
+          label:
+            "Radiant Ring What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $700 to $800 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2Fsnapedit_1719690260501.jpg?alt=media&token=a445aaed-698e-4696-a34a-39e978168f89",
+    },
+    "pear-wedding-ring": {
+      title: "Pear Diamond Wedding Rings",
+      description:
+        "Embrace elegance with our pear-shaped diamond rings, admired for their graceful silhouette and feminine allure. Set in yellow gold, white gold, rose gold, or platinum, these rings are designed to reflect the diamond's natural brilliance and sophistication, making them a perfect symbol of everlasting love and refinement. Each ring is a testament to exceptional craftsmanship and timeless design, ideal for marking life's most cherished moments with elegance and style.",
+      products: products.filter(
+        (product) =>
+          product.categories === "wedding-ring" && product.shape === "pear"
+      ),
+      faqs: [
+        {
+          key: "1",
+          label:
+            "Pear Ring What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $700 to $800 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2Fsnapedit_1719690260501.jpg?alt=media&token=a445aaed-698e-4696-a34a-39e978168f89",
+    },
+  };
