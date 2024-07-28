b9108bf276a15964f6cb9848947bb1f9100d1c18
nhunguyenenn <nguyentnnse170598@fpt.edu.vn>
2024-07-29 06:12:13 +0700

    API - Pagination Product List [FIRM]

--- src/pages/Home/List/FirmList/FirmList.tsx
+++ src/pages/Home/List/FirmList/FirmList.tsx
@@ -1,35 +1,257 @@
 import React, { useState, useEffect } from "react";
-import {
-  Container,
-  List,
-  StyledPagination,
-} from "./FirmList.styled";
+import { Container, List, StyledPagination } from "./FirmList.styled";
 
-import {
-  Card,
-  Col,
-  Row,
-  Typography,
-} from "antd";
+import { Card, Col, Row, Typography } from "antd";
 import { HeartOutlined, HeartFilled } from "@ant-design/icons";
 import { Link, useNavigate, useParams } from "react-router-dom";
 const { Title, Text } = Typography;
 import config from "@/config";
-import { firmData } from "./FirmList.data";
+// import { firmData } from "./FirmList.data";
 import FAQ from "@/components/FAQs/FAQs";
 import Breadcrumb from "@/components/Breadcrumb/Breadcrumb";
 import Banner from "@/components/Banner/Banner";
+import { getImage } from "@/services/imageAPI";
+import { showAllProduct } from "@/services/productAPI";
 
 const FirmList: React.FC = () => {
   const { jewelryFirm } = useParams<{ jewelryFirm: string }>();
   const navigate = useNavigate();
+  const excludedCategories = [
+    "Wedding Ring",
+    "Engagement Ring",
+    "Men Engagement Ring",
+    "Men Wedding Ring",
+  ];
 
+  const [products, setProducts] = useState<any[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await showAllProduct();
+        console.log("API response:", response.data.data);
+
+        if (response && response.data && Array.isArray(response.data.data)) {
+          const fetchedProducts = response.data.data.map((jewelry: any) => ({
+            id: jewelry.ProductID,
+            name: jewelry.Name,
+            brand: jewelry.Brand,
+            totalDiamondPrice: jewelry.TotalDiamondPrice,
+            firstPrice: jewelry.FirstPrice,
+            salePrice: jewelry.SalePrice,
+            type: jewelry.JewelrySetting.jewelryType.Name,
+            jewelryType: jewelry.JewelrySetting?.jewelryType?.Name,
+            images: jewelry.UsingImage.map((image: any) => ({
+              id: image.UsingImageID,
+              url: getImage(image.UsingImageID),
+            })),
+          }));
+
+          console.log(fetchedProducts);
+
+          setProducts(fetchedProducts);
+        } else {
+          console.error("Unexpected API response format:", response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const firmData: Record<string, any> = {
+    Bvlgari: {
+      title: "Bvlgari Firm",
+      description:
+        "Our collection of diamond rings embodies timeless elegance and craftsmanship, each piece meticulously crafted to capture the essence of sophistication and beauty. Whether showcasing the brilliance of round, princess, or cushion-cut diamonds, set in luxurious yellow gold, white gold, rose gold, or platinum settings, each ring tells a story of love and commitment. From classic solitaire designs to intricate halo settings, our rings are designed to celebrate life's most precious moments with enduring style and grace, making them cherished symbols of eternal love and unforgettable milestones.",
+      products: products.filter(
+        (product) =>
+          product.brand === "Bvlgari" &&
+          !excludedCategories.includes(product.jewelryType)
+      ),
+      faqs: [
+        {
+          key: "1",
+          label: "What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $900 to $1000 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2Fbulgari.png?alt=media&token=1897c535-f005-4d37-a106-26bf8426d5ba",
+    },
+    HarryWinston: {
+      title: "Harry Winston Firm",
+      description:
+        "Our collection of diamond rings embodies timeless elegance and craftsmanship, each piece meticulously crafted to capture the essence of sophistication and beauty. Whether showcasing the brilliance of round, princess, or cushion-cut diamonds, set in luxurious yellow gold, white gold, rose gold, or platinum settings, each ring tells a story of love and commitment. From classic solitaire designs to intricate halo settings, our rings are designed to celebrate life's most precious moments with enduring style and grace, making them cherished symbols of eternal love and unforgettable milestones.",
+      products: products.filter(
+        (product) =>
+          product.brand === "Harry Winston" &&
+          !excludedCategories.includes(product.jewelryType)
+      ),
+      faqs: [
+        {
+          key: "1",
+          label: "What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $700 to $800 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2Flearnaboutbanner.jpeg?alt=media&token=e7168cf6-8434-4cad-95e8-dc5fce8d81af",
+    },
+    "Tiffany&Co": {
+      title: "Tiffany & Co Firm",
+      description:
+        "Our collection of diamond rings embodies timeless elegance and craftsmanship, each piece meticulously crafted to capture the essence of sophistication and beauty. Whether showcasing the brilliance of round, princess, or cushion-cut diamonds, set in luxurious yellow gold, white gold, rose gold, or platinum settings, each ring tells a story of love and commitment. From classic solitaire designs to intricate halo settings, our rings are designed to celebrate life's most precious moments with enduring style and grace, making them cherished symbols of eternal love and unforgettable milestones.",
+      products: products.filter(
+        (product) =>
+          product.brand === "Tiffany & Co" &&
+          !excludedCategories.includes(product.jewelryType)
+      ),
+      faqs: [
+        {
+          key: "1",
+          label: "What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $500 to $600 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2FDesign%20Your%20Own%20Pendant.jpeg?alt=media&token=9e6ba197-39d3-4b7e-84f3-a185792eb4aa",
+    },
+    Cartier: {
+      title: "Cartier Firm",
+      description:
+        "Our collection of diamond rings embodies timeless elegance and craftsmanship, each piece meticulously crafted to capture the essence of sophistication and beauty. Whether showcasing the brilliance of round, princess, or cushion-cut diamonds, set in luxurious yellow gold, white gold, rose gold, or platinum settings, each ring tells a story of love and commitment. From classic solitaire designs to intricate halo settings, our rings are designed to celebrate life's most precious moments with enduring style and grace, making them cherished symbols of eternal love and unforgettable milestones.",
+      products: products.filter(
+        (product) =>
+          product.brand === "Cartier" &&
+          !excludedCategories.includes(product.jewelryType)
+      ),
+      faqs: [
+        {
+          key: "1",
+          label: "What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $300 to $400 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2Flab_diamonds_sale_banner.6a518.jpg?alt=media&token=b7d30ece-f543-4890-b7de-7c37e1286dfb",
+    },
+    "VanCleef&Arpels": {
+      title: "Van Cleef & Arpels Firm",
+      description:
+        "Our collection of diamond rings embodies timeless elegance and craftsmanship, each piece meticulously crafted to capture the essence of sophistication and beauty. Whether showcasing the brilliance of round, princess, or cushion-cut diamonds, set in luxurious yellow gold, white gold, rose gold, or platinum settings, each ring tells a story of love and commitment. From classic solitaire designs to intricate halo settings, our rings are designed to celebrate life's most precious moments with enduring style and grace, making them cherished symbols of eternal love and unforgettable milestones.",
+      products: products.filter(
+        (product) =>
+          product.brand === "Van Cleef & Arpels" &&
+          !excludedCategories.includes(product.jewelryType)
+      ),
+      faqs: [
+        {
+          key: "1",
+          label: "What is the average cost of a womens diamond wedding ring?",
+          children: (
+            <p>
+              {" "}
+              Our women's diamond rings range from $276 to $56,024 depending on
+              several factors, including the type of metal and diamond carat
+              weight.
+            </p>
+          ),
+        },
+        {
+          key: "2",
+          label: "Can weddings rings be diamond rings?",
+          children: (
+            <p>
+              {" "}
+              Yes, diamond rings make perfect weddings rings and engagement
+              rings.
+            </p>
+          ),
+        },
+      ],
+      bannerImage:
+        "https://firebasestorage.googleapis.com/v0/b/testsaveimage-abb59.appspot.com/o/BannerProductList%2FBrandBvl.jpg?alt=media&token=344f0091-7993-435a-9fc1-e6f689f0e976",
+    },
+  };
   if (!jewelryFirm || !firmData[jewelryFirm]) {
     return <div>Invalid jewelry type selected.</div>;
   }
 
   const currentFirmData = firmData[jewelryFirm];
-
   const [wishList, setWishList] = useState<string[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const pageSize = 8;
@@ -62,7 +284,7 @@ const FirmList: React.FC = () => {
   return (
     <Container>
       <div>
-      <Breadcrumb
+        <Breadcrumb
           items={[
             { title: "Home", href: "/" },
             { title: currentFirmData.title },
@@ -70,46 +292,55 @@ const FirmList: React.FC = () => {
         />
       </div>
       <Banner
-      bannerImage={currentFirmData.bannerImage}
-      title={currentFirmData.title}
-      description={currentFirmData.description}
-    />
+        bannerImage={currentFirmData.bannerImage}
+        title={currentFirmData.title}
+        description={currentFirmData.description}
+      />
       <List>
         <Row gutter={[16, 16]}>
-          {currentFirmData.products.map((product: any) => (
-            <Col key={product.id} span={6}>
-              
+          {currentFirmData.products
+            .slice((currentPage - 1) * pageSize, currentPage * pageSize)
+            .map((product: any) => (
+              <Col key={product.id} span={6}>
                 <Card
                   key={product.id}
                   style={{ borderRadius: "0" }}
                   hoverable
                   className="product-card"
                   cover={
-                    <>
-                    <Link to={`/product/${product.id}`} >
-                      <img
-                        style={{ borderRadius: "0" }}
-                        src={product.images[0]}
-                        alt={product.name}
-                        className="product-image"
-                        onMouseOver={(e) =>
-                          (e.currentTarget.src = product.images[2])
-                        }
-                        onMouseOut={(e) =>
-                          (e.currentTarget.src = product.images[0])
-                        }
-                      />
-                      </Link>
-                      {product.salePrice && (
-                        <div className="sale-badge">SALE</div>
-                      )}
-                    </>
+                    product.images.length > 0 ? (
+                      <>
+                        <Link to={`/product/${product.id}`}>
+                          <img
+                            style={{ borderRadius: "0" }}
+                            src={product.images[0]?.url || ""}
+                            alt={product.name}
+                            className="product-image"
+                            onMouseOver={(e) =>
+                              (e.currentTarget.src =
+                                product.images[1]?.url ||
+                                product.images[0]?.url ||
+                                "")
+                            }
+                            onMouseOut={(e) =>
+                              (e.currentTarget.src =
+                                product.images[0]?.url || "")
+                            }
+                          />
+                        </Link>
+                        {product.salePrice && (
+                          <div className="sale-badge">SALE</div>
+                        )}
+                      </>
+                    ) : (
+                      <div>No Image Available</div>
+                    )
                   }
                 >
                   <div className="product-info">
                     <Title level={4} className="product-name">
-                    <Link to={`/product/${product.id}`} >
-                      <div>{product.name}</div>
+                      <Link to={`/product/${product.id}`}>
+                        <div>{product.name}</div>
                       </Link>
                       {wishList.includes(product.id) ? (
                         <HeartFilled
@@ -125,18 +356,18 @@ const FirmList: React.FC = () => {
                     </Title>
                     <div className="price-container">
                       <Text className="product-price">
-                        ${product.salePrice ? product.salePrice : product.price}
+                        ${product.firstPrice + product.totalDiamondPrice}
                       </Text>
                       {product.salePrice && (
                         <Text delete className="product-sale-price">
-                          ${product.price}
+                          ${product.totalDiamondPrice}
                         </Text>
                       )}
                     </div>
                   </div>
                 </Card>
-            </Col>
-          ))}
+              </Col>
+            ))}
           <Col span={6}>
             <Card className="show-all-card">
               <div className="show-all-content">
@@ -176,7 +407,7 @@ const FirmList: React.FC = () => {
           onChange={onChange}
         />
       </FAQs> */}
-        <FAQ title={currentFirmData.title} faqs={faqs} />
+      <FAQ title={currentFirmData.title} faqs={faqs} />
     </Container>
   );
 };
